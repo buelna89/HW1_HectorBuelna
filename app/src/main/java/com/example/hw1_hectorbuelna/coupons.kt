21c4e3527b7f48be1798831b6a8c70dbdfd01cbb
Hector Buelna <bhrami@hotmail.com>
2022-09-28 13:06:15 -0700

    added the discount & generated discount. Save and Restore to be Done...

--- app/src/main/java/com/example/hw1_hectorbuelna/coupons.kt
+++ app/src/main/java/com/example/hw1_hectorbuelna/coupons.kt
@@ -3,6 +3,9 @@ package com.example.hw1_hectorbuelna
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class coupons : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -10,5 +13,14 @@ class coupons : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coupons)
+
+        findViewById<Button>(R.id.button3).setOnClickListener()
+        {
+            val display = findViewById<TextView>(R.id.textView3)
+
+            display.text = "Discont created"
+
+
+        }
     }
 }
